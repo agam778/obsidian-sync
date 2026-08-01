c3d795da40dc9493232d1caa933a0ab63c03218c
FNS Service <fns@email.com>
2026-08-01 18:46:45 +0800

    Update from Sync Service

--- PSUC/Notes/Lab/Formula based C Programs/sum.c
+++ PSUC/Notes/Lab/Formula based C Programs/sum.c
@@ -6,5 +6,5 @@ int main(void)
     printf("Enter a 4 digit number: ");
     scanf("%1d%1d%1d%1d", &n1, &n2, &n3, &n4);
     sum = n1 + n2 + n3 + n4;
-    printf("Sum: %d", sum);
+    printf("Sum: %d\n", sum);
 }
