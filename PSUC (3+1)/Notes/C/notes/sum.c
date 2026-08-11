9dc80e79ba228b71d62fecc42cb6e8d67d4df82b
FNS Service <fns@email.com>
2026-08-11 16:46:30 +0800

    Update from Sync Service

---
+++ PSUC (3+1)/Notes/C/notes/sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n1, n2;
+    printf("Enter first number: ");
+    scanf("%d", &n1);
+    printf("Enter second number: ");
+    scanf("%d", &n2);
+    printf("Sum: %d\n", n1 + n2);
+}
