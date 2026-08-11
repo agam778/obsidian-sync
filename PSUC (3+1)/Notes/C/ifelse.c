0a79054bb4e207196f7e7dd3d88a72667f2abd1c
FNS Service <fns@email.com>
2026-08-11 16:59:23 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/C/ifelse.c
+++ PSUC (3+1)/Notes/C/ifelse.c
@@ -4,12 +4,14 @@
 int main(void)
 {
     int n1, n2;
+    char yn;
     printf("Enter first number: ");
     scanf("%d", &n1);
     printf("Enter second number: ");
     scanf("%d", &n2);
-    getchar();
+    // getchar();
     printf("y or n: ");
+    scanf("%c", &yn);
     if (getchar() == 'y')
     {
         printf("Sum: %d\n", n1 + n2);
