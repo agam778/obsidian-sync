ce7517b049738a905d63f83dc618af64ad41a74f
FNS Service <fns@email.com>
2026-08-11 16:48:10 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/C/sum copy.c
+++ PSUC (3+1)/Notes/C/sum copy.c
@@ -2,11 +2,15 @@
 
 int main(void)
 {
-    int n1, n2;
+    int n1, n2, c;
     printf("Enter first number: ");
     scanf("%d", &n1);
     printf("Enter second number: ");
     scanf("%d", &n2);
     printf("y or n: ");
-    scanf("%c")
+    scanf("%c", c);
+    if (c == 'y')
+    {
+        printf("Sum: %d\n", n1 + n2);
+    }
 }
