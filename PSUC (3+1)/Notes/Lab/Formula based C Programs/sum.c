a3fbd88e76dbef010141663d1ac8fe5d09f3f990
FNS Service <fns@email.com>
2026-08-14 17:21:15 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/Lab/Formula based C Programs/sum.c
+++ PSUC (3+1)/Notes/Lab/Formula based C Programs/sum.c
@@ -1,32 +1,32 @@
-// #include <stdio.h>
-
-// int main(void)
-// {
-//     int n1, n2, n3, n4, sum;
-//     printf("Enter a 4-digit number: ");
-//     scanf("%1d%1d%1d%1d", &n1, &n2, &n3, &n4);
-//     sum = n1 + n2 + n3 + n4;
-//     printf("Sum: %d", sum);
-// }
-
 #include <stdio.h>
 
 int main(void)
 {
-    int n, sum = 0;
-
+    int n1, n2, n3, n4, sum;
     printf("Enter a 4-digit number: ");
-    scanf("%d", &n);
-
-    sum += n % 10;
-    n /= 10;
-    sum += n % 10;
-    n /= 10;
-    sum += n % 10;
-    n /= 10;
-    sum += n % 10;
-
-    printf("Sum of digits = %d\n", sum);
-
-    return 0;
+    scanf("%1d%1d%1d%1d", &n1, &n2, &n3, &n4);
+    sum = n1 + n2 + n3 + n4;
+    printf("Sum: %d", sum);
 }
+
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     int n, sum = 0;
+
+//     printf("Enter a 4-digit number: ");
+//     scanf("%d", &n);
+
+//     sum += n % 10;
+//     n /= 10;
+//     sum += n % 10;
+//     n /= 10;
+//     sum += n % 10;
+//     n /= 10;
+//     sum += n % 10;
+
+//     printf("Sum of digits = %d\n", sum);
+
+//     return 0;
+// }
