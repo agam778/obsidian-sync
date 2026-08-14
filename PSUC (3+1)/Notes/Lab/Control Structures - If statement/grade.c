2f724410b26abf1160a7ca290a461c55bc8e82c2
FNS Service <fns@email.com>
2026-08-14 17:08:19 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/Lab/Control Structures - If statement/grade.c
+++ PSUC (3+1)/Notes/Lab/Control Structures - If statement/grade.c
@@ -7,27 +7,20 @@ int main(void)
     scanf("%d", &g);
     printf("Grade: ");
     if (g < 3)
-    {
         printf("F");
-    }
     else if (g == 3 || g == 4)
-    {
         printf("D");
-    }
+
     else if (g == 5 || g == 6)
-    {
+
         printf("C");
-    }
+
     else if (g == 7 || g == 8)
-    {
+
         printf("B");
-    }
+
     else if (g == 9 || g == 10)
-    {
         printf("A");
-    }
     else
-    
         printf("Invalid grade");
-}
 }
