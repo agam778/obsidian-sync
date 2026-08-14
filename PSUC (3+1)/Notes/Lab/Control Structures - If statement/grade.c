ae6fdd78c233a4ad1deafe446b4a8deb4ef15d5c
FNS Service <fns@email.com>
2026-08-14 17:04:33 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/Lab/Control Structures - If statement/grade.c
+++ PSUC (3+1)/Notes/Lab/Control Structures - If statement/grade.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
     int g;
     printf("Enter grade: ");
     scanf("%d", &g);
     printf("Grade: ");
-    if (g<3) {
+    if (g < 3)
+    {
         printf("F");
-    } else if ()
+    }
+    else if (g == 3 || g == 4)
+    {
+        printf("D");
+    }
+    else if ()
 }
