46550adfa780a73a0c3489305491693fed8e8027
FNS Service <fns@email.com>
2026-08-16 02:02:09 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/Lab/Formula based C Programs/distance.c
+++ PSUC (3+1)/Notes/Lab/Formula based C Programs/distance.c
@@ -2,15 +2,12 @@
 
 int main(void)
 {
-    float d, cm, inch, feet;
+    float mm = 269.32, cm, inch, feet;
 
-    printf("Enter distance (in mm): ");
-    scanf("%f", &d);
-
-    cm = d / 10.0;
+    cm = mm / 10.0;
     inch = cm / 2.5;
     feet = inch / 12.0;
 
-    printf("cm: %.2f, inch: %.2f, feet: %.2f\n", cm, inch, feet);
+    printf("cm: %.2f\ninch: %.2f\nfeet: %.2f\n", cm, inch, feet);
     return 0;
 }
