cbeeb6c9d7651a47b71201069b69fc5bfc5ddc03
FNS Service <fns@email.com>
2026-08-16 02:39:30 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/Lab/Formula based C Programs/swap2.c
+++ PSUC (3+1)/Notes/Lab/Formula based C Programs/swap2.c
@@ -3,9 +3,9 @@
 int main(void)
 {
     int a = 5, b = 7;
-    a = a + b; // 5+7 = 12
-    b = a - b; // 12-7 = 5
-    a = a - b; // 12-5 = 7
+    a = a + b;
+    b = a - b;
+    a = a - b;
     printf("%d, %d", a, b);
     return 0;
 }
