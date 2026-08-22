a0e7c44359de4e522facbc0236ddfc6a7295f557
FNS Service <fns@email.com>
2026-08-22 19:13:48 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/Lab/Control Structures - If statement/days.c
+++ PSUC (3+1)/Notes/Lab/Control Structures - If statement/days.c
@@ -2,41 +2,35 @@
 
 int main(void)
 {
-    int y, m, d = 0;
+    int y, m, d;
+
     printf("Enter year: ");
     scanf("%d", &y);
-    printf("Enter no. of months: ");
-    scanf(" %d", &m);
-    if (m < 1 || m > 12)
-        printf("Enter valid no. of months.");
+    printf("Enter month: ");
+    scanf("%d", &m);
 
-    if (m >= 1)
-        d += 31;
-    if (m >= 2)
-        if ((y % 4 == 0) && (y % 100 != 0) || (y % 400 == 0))
-            d += 29;
+    if (m < 1 || m > 12)
+    {
+        printf("Enter valid month.");
+        return 0;
+    }
+
+    if (m == 2)
+    {
+        if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
+            d = 29;
         else
-            d += 28;
-    if (m >= 3)
-        d += 31;
-    if (m >= 4)
-        d += 30;
-    if (m >= 5)
-        d += 31;
-    if (m >= 6)
-        d += 30;
-    if (m >= 7)
-        d += 31;
-    if (m >= 8)
-        d += 31;
-    if (m >= 9)
-        d += 30;
-    if (m >= 10)
-        d += 31;
-    if (m >= 11)
-        d += 30;
-    if (m == 12)
-        d += 31;
+            d = 28;
+    }
+    else if (m == 4 || m == 6 || m == 9 || m == 11)
+    {
+        d = 30;
+    }
+    else
+    {
+        d = 31;
+    }
 
     printf("Number of days: %d", d);
+    return 0;
 }
