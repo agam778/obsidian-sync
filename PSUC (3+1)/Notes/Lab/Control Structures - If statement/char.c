1ea681378cc43a1f7e66bbc1b15e4592d97535b8
FNS Service <fns@email.com>
2026-08-22 19:44:10 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/Lab/Control Structures - If statement/char.c
+++ PSUC (3+1)/Notes/Lab/Control Structures - If statement/char.c
@@ -6,7 +6,7 @@ int main(void)
     printf("Enter a character: ");
     scanf("%c", &c);
 
-    if (c == '1' || c == '2' || c == '3' || c=='4' || c=='5')
+    if (c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9')
     {
         printf("%c is a digit", c);
     }
