e1b596efcbdcf956bfb2a563ac8add9f245ffc69
FNS Service <fns@email.com>
2026-08-22 19:38:50 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/Lab/Control Structures - If statement/area.c
+++ PSUC (3+1)/Notes/Lab/Control Structures - If statement/area.c
@@ -4,12 +4,20 @@
 int main(void)
 {
     float a, b, c, s, area;
+
     printf("Enter three sides: ");
     scanf("%f%f%f", &a, &b, &c);
+
     if ((a + b > c) && (b + c > a) && (a + c > b))
     {
-        s = ((a + b + c) / 2);
-        area = sqrt()
+        s = (a + b + c) / 2.0;
+        area = sqrt(s * (s - a) * (s - b) * (s - c));
         printf("Area of triangle: %.2f", area);
     }
+    else
+    {
+        printf("These sides do not form a triangle.");
+    }
+
+    return 0;
 }
