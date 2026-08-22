4aebec67793e180f1e417d1371ab0970388e0a0d
FNS Service <fns@email.com>
2026-08-22 18:47:06 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/Lab/Control Structures - If statement/roots.c
+++ PSUC (3+1)/Notes/Lab/Control Structures - If statement/roots.c
@@ -7,15 +7,13 @@ int main(void)
     float d = b * b - 4 * a * c;
 
     if (d == 0)
-    {
-        float x = -b / (2 * a);
-        printf("Root is: %.2f\n", x);
-    }
+        printf("Root is: %.2f\n", -b / (2 * a));
     else if (d > 0)
     {
         float x1 = (-b + sqrt(d)) / (2 * a);
         float x2 = (-b - sqrt(d)) / (2 * a);
-        printf("Roots are: %.2f, %.2f\n", x1, x2);
+        printf("Roots are: %.2f, %.2f\n", (-b + sqrt(d)) / (2 * a);
+        float x2 = (-b - sqrt(d)) / (2 * a));
     }
     else
     {
