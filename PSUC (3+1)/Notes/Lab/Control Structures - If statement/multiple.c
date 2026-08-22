a9282b511b8f5894316d6ed5403e38ab0a887ff6
FNS Service <fns@email.com>
2026-08-22 19:52:01 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/Lab/Control Structures - If statement/multiple.c
+++ PSUC (3+1)/Notes/Lab/Control Structures - If statement/multiple.c
@@ -11,6 +11,6 @@ int main(void)
     else if (n2 % n1 == 0)
         printf("%d is a multiple of %d", n2, n1);
     else
-        printf("%d and %d are not multiples of each other.");
+        printf("%d and %d are not multiples of each other.", n1, n2);
     return 0;
 }
