09c199ac65f56383e2e3b69056059d9d512d36b6
FNS Service <fns@email.com>
2026-08-22 19:38:21 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/Lab/Control Structures - If statement/area.c
+++ PSUC (3+1)/Notes/Lab/Control Structures - If statement/area.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
-    float a, b, c, area;
+    float a, b, c, s, area;
     printf("Enter three sides: ");
     scanf("%f%f%f", &a, &b, &c);
     if ((a + b > c) && (b + c > a) && (a + c > b))
     {
-        area = ((a + b + c) / 2);
+        s = ((a + b + c) / 2);
+        area = sqrt()
         printf("Area of triangle: %.2f", area);
     }
 }
