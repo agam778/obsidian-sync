8a9268fd3c8bb14c14e93c4c1f956593a039eb1f
FNS Service <fns@email.com>
2026-08-22 19:08:40 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/Lab/Control Structures - If statement/days.c
+++ PSUC (3+1)/Notes/Lab/Control Structures - If statement/days.c
@@ -2,16 +2,28 @@
 
 int main(void)
 {
-    int y, m, d=0;
+    int y, m, d = 0;
     printf("Enter year: ");
     scanf("%d", y);
     printf("Enter no. of months: ");
     scanf("%d", m);
     if (m < 1 || m > 12)
         printf("Enter valid no. of months.");
-    
+
     if (m >= 1)
-        d+=31;
+        d += 31;
     if (m >= 2)
-        if ((y%4==0) && (y%100!=0) || (yea))
+        if ((y % 4 == 0) && (y % 100 != 0) || (y % 400 == 0))
+            d += 29;
+        else
+            d += 28;
+    if (m >= 3)
+        d += 31;
+    if (m >= 4)
+        d += 30;
+    if (m>=5)
+        d+=31;
+    if (m>=6)
+        d+=30;
+        
 }
