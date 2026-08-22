7757bdbce7545ab411faf8b1b9555c8a27ede88c
FNS Service <fns@email.com>
2026-08-22 18:42:22 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/Lab/Control Structures - If statement/roots.c
+++ PSUC (3+1)/Notes/Lab/Control Structures - If statement/roots.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
-int main(void) {
-    int a=2, b=1, c=5;
-    int d=b*b-4*a*c;
+int main(void)
+{
+    float a = 2, b = 1, c = 5;
+    float d = b * b - 4 * a * c;
 
-    if (d==0)
-        printf("")
+    if (d == 0)
+        printf("Roots are: %f", )
 }
