afed09f38b31547a8e5d9ccccf26d15237551fba
FNS Service <fns@email.com>
2026-08-22 20:14:16 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/Lab/Control Structures - Switch/telephone.c
+++ PSUC (3+1)/Notes/Lab/Control Structures - Switch/telephone.c
@@ -8,5 +8,8 @@ int main(void)
     printf("Enter calls: ");
     scanf("%d", calls);
 
-    switch(calls <= 150 ? 0 : calls <= 250 ? 0.9 : calls <= 400 ? 1.2 : )
+    switch(calls <= 150 ? 0 : calls <= 250 ? 0.9 : calls <= 400 ? 1.2 : 1.5) {
+        case 0:
+        case 
+    }
 }
