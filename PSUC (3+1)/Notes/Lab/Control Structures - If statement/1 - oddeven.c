f3a94c965c9477076d00b8c26a022e87c85a9cbc
FNS Service <fns@email.com>
2026-08-27 22:29:55 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/Lab/Control Structures - If statement/1 - oddeven.c
+++ PSUC (3+1)/Notes/Lab/Control Structures - If statement/1 - oddeven.c
@@ -7,11 +7,11 @@ int main(void)
     scanf("%d", &n);
     if (n % 2 == 0)
     {
-        printf("%d is even", n);
+        printf("%d is even\n", n);
     }
     else
     {
-        printf("%d is odd", n);
+        printf("%d is odd\n", n);
     }
     return 0;
 }
--- PSUC (3+1)/Notes/Lab/Control Structures - If statement/2 - leapyear.c
+++ PSUC (3+1)/Notes/Lab/Control Structures - If statement/2 - leapyear.c
@@ -8,6 +8,6 @@ int main(void)
     if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
         printf("%d is a leap year.", y);
     else
-        printf("%d is not a leap year.", y);
+        printf("%d is not a leap year.\n", y);
     return 0;
 }
