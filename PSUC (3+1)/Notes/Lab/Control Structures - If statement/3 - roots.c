31b12fe5ee27693f27dfa3b7d0aa38d593bd08ef
FNS Service <fns@email.com>
2026-08-27 22:37:10 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/Lab/Control Structures - If statement/3 - roots.c
+++ PSUC (3+1)/Notes/Lab/Control Structures - If statement/3 - roots.c
@@ -3,17 +3,7 @@
 
 int main(void)
 {
-    float a, b, c;
-
-    printf("Enter coefficients a, b, and c: ");
-    scanf("%f %f %f", &a, &b, &c);
-
-    if (a == 0)
-    {
-        printf("'a' cannot be 0 in a quadratic equation.\n");
-        return 1;
-    }
-
+    float a = 1, b = 1, c = -5;
     float d = b * b - 4 * a * c;
 
     if (d == 0)
@@ -22,6 +12,5 @@ int main(void)
         printf("Roots are: %.2f, %.2f\n", (-b + sqrt(d)) / (2 * a), (-b - sqrt(d)) / (2 * a));
     else
         printf("There are no real roots.\n");
-
     return 0;
 }
