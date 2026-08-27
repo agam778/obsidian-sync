cd2b07cfe7ed9459016e6212be23a7ab69e64c96
FNS Service <fns@email.com>
2026-08-27 22:57:38 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/Lab/Control Structures - If statement/8 - char.c
+++ PSUC (3+1)/Notes/Lab/Control Structures - If statement/8 - char.c
@@ -4,23 +4,23 @@ int main(void)
 {
     char c;
     printf("Enter a character: ");
-    scanf("%c", &c);
+    scanf(" %c", &c);
 
     if (c >= '0' && c <= '9')
     {
-        printf("%c is a digit", c);
+        printf("%c is a digit\n", c);
     }
     else if (c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' || c == 'I' || c == 'o' || c == 'O' || c == 'u' || c == 'U')
     {
-        printf("%c is a vowel", c);
+        printf("%c is a vowel\n", c);
     }
     else if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
     {
-        printf("%c is a consonant", c);
+        printf("%c is a consonant\n", c);
     }
     else
     {
-        printf("%c is not a digit, vowel or consonant", c);
+        printf("%c is not a digit, vowel or consonant\n", c);
     }
 
     return 0;
