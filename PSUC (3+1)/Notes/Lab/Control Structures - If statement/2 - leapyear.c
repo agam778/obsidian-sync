83808b4c1c000e7b8f7b4485118eeab28b2f268e
FNS Service <fns@email.com>
2026-08-29 14:21:08 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/Lab/Control Structures - If statement/2 - leapyear.c
+++ PSUC (3+1)/Notes/Lab/Control Structures - If statement/2 - leapyear.c
@@ -6,7 +6,7 @@ int main(void)
     printf("Enter year: ");
     scanf("%d", &y);
     if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
-        printf("%d is a leap year.", y);
+        printf("%d is a leap year.\n", y);
     else
         printf("%d is not a leap year.\n", y);
     return 0;
