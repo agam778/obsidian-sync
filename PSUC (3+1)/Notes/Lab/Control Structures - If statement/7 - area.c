92c3ce6f25c4281344200a2f8ff94068791d0d76
FNS Service <fns@email.com>
2026-08-29 15:35:13 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/Lab/Control Structures - If statement/7 - area.c
+++ PSUC (3+1)/Notes/Lab/Control Structures - If statement/7 - area.c
@@ -3,27 +3,20 @@
 
 int main(void)
 {
-    float a, b, c, s, area;
+    float a, b, c, area;
 
     printf("Enter three sides: ");
     scanf("%f%f%f", &a, &b, &c);
 
-    if (a <= 0 || b <= 0 || c <= 0)
+    if (a > 0 && b > 0 && c > 0 
+        && (a + b > c) && (b + c > a) && (a + c > b))
     {
-        printf("Sides must be positive.\n");
-        return 0;
-    }
-
-    if ((a + b > c) && (b + c > a) && (a + c > b))
-    {
-        s = (a + b + c) / 2.0;
+        float s = (a + b + c) / 2.0;
         area = sqrt(s * (s - a) * (s - b) * (s - c));
         printf("Area of triangle: %.2f\n", area);
     }
     else
-    {
-        printf("These sides do not form a triangle.\n");
-    }
+        printf("Invalid triangle.\n");
 
     return 0;
 }
