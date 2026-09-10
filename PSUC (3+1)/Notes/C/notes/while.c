c6e744db71f28af21242ed5eb8f2754d0dfadfd4
FNS Service <fns@email.com>
2026-09-10 17:16:57 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/C/notes/while.c
+++ PSUC (3+1)/Notes/C/notes/while.c
@@ -129,13 +129,11 @@ int main(void)
     int n = 7;
     for (int i = 1; i <= n; i++)
     {
+        for (int s = 1; s <= n - i; s++)
+            printf(" ");
         for (int j = 1; j <= i; j++)
             printf("*");
         printf("\n");
     }
 }
 
-int main(void)
-{
-
-}
