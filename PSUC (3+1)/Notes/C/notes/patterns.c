1c06400c91949c72260eb60911cbda32a19017ac
FNS Service <fns@email.com>
2026-09-11 16:42:21 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/C/notes/patterns.c
+++ PSUC (3+1)/Notes/C/notes/patterns.c
@@ -33,13 +33,24 @@
 //     }
 // }
 
+// int main(void)
+// {
+//     int n = 5;
+//     for (int i = n; i >= 1; i--)
+//     {
+//         for (int j = i; j >= 1; j--)
+//             printf("%d", j);
+//         printf("\n");
+//     }
+// }
+
 int main(void)
 {
     int n = 5;
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j <= i; j++)
-            printf("%d", j);
+        for (int j = n; j >= 1; j--)
+            printf("%d", i);
         printf("\n");
     }
 }
