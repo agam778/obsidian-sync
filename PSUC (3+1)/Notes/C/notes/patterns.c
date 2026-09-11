b1b27fa071bacd601a1478f143b5d41942c30d55
FNS Service <fns@email.com>
2026-09-11 16:23:56 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/C/notes/patterns.c
+++ PSUC (3+1)/Notes/C/notes/patterns.c
@@ -5,10 +5,8 @@ int main(void)
     int n = 7;
     for (int i = 1; i <= n; i++)
     {
-        for (int s = 1; s <= n - i; s++)
-            printf(" ");
         for (int j = 1; j <= i; j++)
-            printf("*");
+            printf("%d", j);
         printf("\n");
     }
 }
