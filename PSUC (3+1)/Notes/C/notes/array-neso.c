3dd8629d369cd1bf2bbe6949ebd88dfb405954b2
FNS Service <fns@email.com>
2026-09-13 14:07:54 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/C/notes/array-neso.c
+++ PSUC (3+1)/Notes/C/notes/array-neso.c
@@ -4,9 +4,10 @@
 int main(void)
 {
     int arr[N] = {34, 56, 54, 32, 67, 89, 90, 32, 21};
-    for (int i = 1; i <= N; i++)
-    {
-        printf("%d ", arr[N - i]);
-    }
+    // for (int i = 1; i <= N; i++)
+    // {
+    //     printf("%d ", arr[N - i]);
+    // }
+    for (int i = N; i > 0, i--)
     printf("\n");
 }
