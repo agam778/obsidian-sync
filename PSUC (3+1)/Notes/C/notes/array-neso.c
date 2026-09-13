87b129befdc6a2c6e6122ecd8f373c7c11bedd73
FNS Service <fns@email.com>
2026-09-13 14:05:52 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/C/notes/array-neso.c
+++ PSUC (3+1)/Notes/C/notes/array-neso.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
-#define N 2
+#define N 9
 
 int main(void)
 {
-    int n;
-    // for (int i = 0; i < N; i++)
-    // {
-    //     printf("Enter number %d: ", i + 1);
-    //     scanf("%d", &n);
-    //     arr[i] = n;
-    // }
-    // printf("\n");
-    int arr[] = {1, 2, 3, 4, [3] = 67, [7] = 89};
-    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+    int arr[N] = {34, 56, 54, 32, 67, 89, 90, 32, 21};
+    for (int i = 0; i < N; i++)
     {
-        printf("Number %d: %d\n", i + 1, arr[i]);
+        printf("%d ", );
     }
 }
