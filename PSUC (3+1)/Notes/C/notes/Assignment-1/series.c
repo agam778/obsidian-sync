e5c2ef58eb06162acbb78c4baf2dcfdfbe2fc74f
FNS Service <fns@email.com>
2026-09-13 18:25:52 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/C/notes/Assignment-1/series.c
+++ PSUC (3+1)/Notes/C/notes/Assignment-1/series.c
@@ -10,7 +10,7 @@ int main(void)
     scanf("%d", &n);
     for (int i = 0; i <= n; i++)
     {
-        sum = sum + pow(a, i);
+        sum = pow(a, i);
         printf("%d\n", sum);
     }
     printf("%d\n", sum);
