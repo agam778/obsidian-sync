95dfd7410d8666488321e631e49cdf8b129ceec0
FNS Service <fns@email.com>
2026-09-13 18:14:45 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/C/notes/armstrong.c
+++ PSUC (3+1)/Notes/C/notes/armstrong.c
@@ -3,9 +3,19 @@
 
 int main(void)
 {
-    int n, sum, d;
+    int n, n1, sum, d;
     printf("Enter a number: ");
     scanf("%d", &n);
-
+    n1 = n;
     for (int i = 0; i < 3; i++)
+    {
+        d = n % 10;
+        sum = sum + d * d * d;
+        n = n / 10;
+    }
+
+    if (sum == n1)
+        printf("%d is an armstrong number");
+    else 
+        printf("%d is not an armstrong number");
 }
