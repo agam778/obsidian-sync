d2d716bca478f108479b26377ad82fefe01e083e
FNS Service <fns@email.com>
2026-09-13 18:21:31 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/C/notes/Assignment-1/series.c
+++ PSUC (3+1)/Notes/C/notes/Assignment-1/series.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
-    int n, a;
+    int n, a, sum;
     printf("Enter value of a: ");
     scanf("%d", &a);
-    printf("Enter value of n: " );
+    printf("Enter value of n: ");
     scanf("%d", &n);
-    for (int i = 0; )
+    for (int i = 0; i <= n; i++)
+    {
+        ;
+    }
 }
