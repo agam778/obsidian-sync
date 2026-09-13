31b46856a717924e5c55ea9848e79483d1f918f0
FNS Service <fns@email.com>
2026-09-13 15:51:25 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/C/notes/array-jenny.c
+++ PSUC (3+1)/Notes/C/notes/array-jenny.c
@@ -2,6 +2,8 @@
 
 int main(void)
 {
-    int arr[10] = {67, 69, 420, 69420};
+    int d, p, arr[10] = {67, 69, 420, 69420};
+    printf("Enter data and position: ");
+    scanf("%d%d", &d, &p);
     
 }
