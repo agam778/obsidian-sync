44d96093b7c800d3aa83926fc7724735aea854b8
FNS Service <fns@email.com>
2026-09-13 15:54:39 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/C/notes/array-jenny.c
+++ PSUC (3+1)/Notes/C/notes/array-jenny.c
@@ -5,5 +5,9 @@ int main(void)
     int d, p, arr[10] = {67, 69, 420, 69420};
     printf("Enter data and position: ");
     scanf("%d%d", &d, &p);
-    for (int i = 9; )
+    for (int i = 9; i > p - 1; i--)
+        arr[i + 1] = arr[i];
+    arr[p - 1] = d;
+    for (int i = 0; i > 10; i++)
+        printf("")
 }
