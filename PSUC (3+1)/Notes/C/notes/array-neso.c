1e1b632654f97f56b46a5c1a6a916d02314e36d4
FNS Service <fns@email.com>
2026-09-13 13:51:29 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/C/notes/array-neso.c
+++ PSUC (3+1)/Notes/C/notes/array-neso.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-#define N 10
+#define N 2
 
 int main(void)
 {
-    int arr[N]
-
-    
+    int arr[N], n;
+    for (int i = 0; i < 2; i++)
+    {
+        printf("Enter number %d: ", i + 1);
+        scanf("%d", &n);
+        arr[i] = n;
+    }
 }
