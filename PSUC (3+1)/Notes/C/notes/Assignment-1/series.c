e2cd93ffac3a2187fc3c5e88e0f4e9a4a0f7b040
FNS Service <fns@email.com>
2026-09-13 18:20:50 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/C/notes/Assignment-1/series.c
+++ PSUC (3+1)/Notes/C/notes/Assignment-1/series.c
@@ -2,5 +2,10 @@
 
 int main(void)
 {
-    
+    int n, a;
+    printf("Enter value of a: ");
+    scanf("%d", &a);
+    printf("Enter value of n: " );
+    scanf("%d", &n);
+    for (int i = 0; )
 }
