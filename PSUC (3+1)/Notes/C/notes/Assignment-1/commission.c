1d6eab36fea26479be4ec21b5945966c5862dfeb
FNS Service <fns@email.com>
2026-09-13 18:38:06 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/C/notes/Assignment-1/commission.c
+++ PSUC (3+1)/Notes/C/notes/Assignment-1/commission.c
@@ -7,5 +7,11 @@ int main(void)
     scanf("%f", &s);
     if (s >= 0 && s <= 5000)
         c = s;
-    else if (s>=5001 && s<=)
+    else if (s >= 5001 && s <= 12000)
+        c = s * 3.0;
+    else if (s>=12001 && s<=22000)
+        c = s * 7.0;
+    else if (s>=22001 && s<=30000)
+        c = s * 10.0;
+    else ()
 }
