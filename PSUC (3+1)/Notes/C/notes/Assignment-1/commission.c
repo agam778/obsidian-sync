a1729ec9f52e8bf0d6e20a80f1b28e5622bf56c5
FNS Service <fns@email.com>
2026-09-13 18:39:06 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/C/notes/Assignment-1/commission.c
+++ PSUC (3+1)/Notes/C/notes/Assignment-1/commission.c
@@ -8,10 +8,15 @@ int main(void)
     if (s >= 0 && s <= 5000)
         c = s;
     else if (s >= 5001 && s <= 12000)
-        c = s * 3.0;
-    else if (s>=12001 && s<=22000)
+        c = (s * 3.0;
+    else if (s >= 12001 && s <= 22000)
         c = s * 7.0;
-    else if (s>=22001 && s<=30000)
+    else if (s >= 22001 && s <= 30000)
         c = s * 10.0;
-    else ()
+    else if (s >= 30001)
+        c = s * 15.0;
+    else
+        printf("Please enter a valid number.");
+
+    printf("Commission: ")
 }
