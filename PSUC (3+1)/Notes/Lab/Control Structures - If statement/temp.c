6beb2e81ce29d408ad059c991134d1c7d5b0e68e
FNS Service <fns@email.com>
2026-09-24 21:24:40 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/Lab/Control Structures - If statement/temp.c
+++ PSUC (3+1)/Notes/Lab/Control Structures - If statement/temp.c
@@ -4,11 +4,13 @@ int main()
     int n;
 
     int sum;
-    sum = 0; // initialize sum
+    sum = 0;
 
     for (n = 1; n < 100; n = n + 1)
-
+    {
+        printf("%d\n", n);
         sum = sum + n;
+    }
 
     printf("%d", sum);
     return 0;
