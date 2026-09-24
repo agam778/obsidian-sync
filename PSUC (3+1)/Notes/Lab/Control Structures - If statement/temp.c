924e573eb34b20ac71fb0ffea139797bdb19e670
FNS Service <fns@email.com>
2026-09-25 01:05:19 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/Lab/Control Structures - If statement/temp.c
+++ PSUC (3+1)/Notes/Lab/Control Structures - If statement/temp.c
@@ -2,10 +2,13 @@
 
 int main()
 {
-    int m, y;
+    int m, y, d = 0;
     printf("Enter months: ");
     scanf("%d", &m);
     printf("Enter year: ");
     scanf("%d", &y);
-    
+    if (m >= 1)
+        d += 31;
+    if (m >= 2)
+        
 }
