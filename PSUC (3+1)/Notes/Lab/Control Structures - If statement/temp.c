1264b2e7652e9dc81e1bc2c69ac9ce0ca663327e
FNS Service <fns@email.com>
2026-09-24 21:18:30 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/Lab/Control Structures - If statement/temp.c
+++ PSUC (3+1)/Notes/Lab/Control Structures - If statement/temp.c
@@ -7,7 +7,7 @@ int main()
     printf("TABLE OF TRIANGULAR NUMBERS\n");
     printf("n       Sum from 1 to n\n");
 
-    for (n = 1; n <= 10; n++)
+    for (n = 1;; n++)
     {
         triangularNumber += n;
         printf("%2d       %d\n", n, triangularNumber);
