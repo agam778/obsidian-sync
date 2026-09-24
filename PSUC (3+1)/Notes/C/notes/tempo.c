880f2614645038380a7832d12c96602b38ff55d0
FNS Service <fns@email.com>
2026-09-24 20:07:56 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/C/notes/tempo.c
+++ PSUC (3+1)/Notes/C/notes/tempo.c
@@ -4,14 +4,5 @@ int main()
 {
     int a = 10, b = 12, c = 14;
     if (a < b)
-    {
-        if (a < c)
-        {
-            printf("a is smallest");
-        }
-        else
-        {
-            printf("c is smallest");
-        }
-    } 
+     
 }
