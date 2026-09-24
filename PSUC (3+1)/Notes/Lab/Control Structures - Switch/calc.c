26d7d3ab6bd3f77624fe413333807203f3b6e579
FNS Service <fns@email.com>
2026-09-25 01:33:49 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/Lab/Control Structures - Switch/calc.c
+++ PSUC (3+1)/Notes/Lab/Control Structures - Switch/calc.c
@@ -4,5 +4,12 @@ int main()
 {
     float a, b;
     char op;
-    
+    printf("Enter number operator number: ");
+    scanf("%f%c%f", &a, &op, &b);
+    switch (op)
+    {
+    case '+':
+        printf("%.2f", a + b);
+        break;
+    }
 }
