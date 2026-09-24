3dbe2f5fd5a2dbd5b351b4e02bc8fcb0ab38a471
FNS Service <fns@email.com>
2026-09-24 20:54:23 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/Lab/Control Structures - If statement/3 - roots.c
+++ PSUC (3+1)/Notes/Lab/Control Structures - If statement/3 - roots.c
@@ -3,16 +3,6 @@
 
 int main(void)
 {
-    float a = 1, b = 1, c = -5;
-    float d = b * b - 4 * a * c;
-
-    if (d == 0)
-        printf("Root is: %.2f\n", -b / (2 * a));
-    else if (d > 0)
-        printf("Roots are: %.2f, %.2f\n",
-                (-b + sqrt(d)) / (2 * a),
-                (-b - sqrt(d)) / (2 * a));
-    else
-        printf("There are no real roots.\n");
-    return 0;
+    int a = 5;
+    if (a < 4)
 }
