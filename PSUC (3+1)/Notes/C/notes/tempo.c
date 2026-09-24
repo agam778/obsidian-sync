9fd5561e789221d7e0450e71fd2c36b60d93177c
FNS Service <fns@email.com>
2026-09-24 20:00:20 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/C/notes/tempo.c
+++ PSUC (3+1)/Notes/C/notes/tempo.c
@@ -4,4 +4,14 @@ int main()
 {
     int age = 5;
     if (age < 10)
+    {
+        if (age < 7)
+        {
+            printf("Less than 7");
+        }
+        else
+        {
+            printf("greater than 7, less than 10");
+        }
+    }
 }
