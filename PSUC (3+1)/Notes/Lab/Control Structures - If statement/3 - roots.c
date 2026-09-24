1aae037e5ad19e9a4765d26a8deaf7bf162765bd
FNS Service <fns@email.com>
2026-09-24 20:55:04 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/Lab/Control Structures - If statement/3 - roots.c
+++ PSUC (3+1)/Notes/Lab/Control Structures - If statement/3 - roots.c
@@ -6,7 +6,11 @@ int main(void)
     int a = 5;
     if (a < 4)
         printf('f');
-    else if ( a == 3)
-        printf("fals");
     else if (a == 3)
+        printf("fals");
+    else if (a == 2)
+
+        printf("falseee");
+    else
+        printf("sss");
 }
