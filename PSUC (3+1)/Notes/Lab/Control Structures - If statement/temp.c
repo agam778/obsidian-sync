21410dee6defb370fd0560f2ef081450ba4b96a2
FNS Service <fns@email.com>
2026-09-25 01:03:38 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/Lab/Control Structures - If statement/temp.c
+++ PSUC (3+1)/Notes/Lab/Control Structures - If statement/temp.c
@@ -1,17 +1,7 @@
 #include <stdio.h>
+
 int main()
 {
-    int n;
-
-    int sum;
-    sum = 0;
-
-    for (n = 1; n < 100; n = n + 1)
-    {
-        printf("%d\n", n);
-        sum = sum + n;
-    }
-
-    printf("%d", sum);
-    return 0;
+    int m, y;
+    printf("Enter months: ");
 }
