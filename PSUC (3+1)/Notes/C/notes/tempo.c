cbfe9da8be8a733db4a63361e3258ac1a6a378e0
FNS Service <fns@email.com>
2026-09-24 13:19:03 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/C/notes/tempo.c
+++ PSUC (3+1)/Notes/C/notes/tempo.c
@@ -5,6 +5,8 @@ int main()
     int n1, n2, sum;
     printf("Enter first number: ");
     scanf("%d", &n1);
-    printf("")
+    printf("Enter second number: ");
+    scanf("%d", &n2);
+    sum = n1 + n2;
     printf("%d", sum);
 }
