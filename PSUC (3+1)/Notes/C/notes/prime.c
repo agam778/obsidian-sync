ce5762ef60cf3c162576598b2ba8a24275d859b0
FNS Service <fns@email.com>
2026-09-25 11:43:26 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/C/notes/prime.c
+++ PSUC (3+1)/Notes/C/notes/prime.c
@@ -1,1 +1,19 @@
-// nth prime
+#include <stdio.h>
+
+int main(void)
+{
+    int n = 4, count = 0;
+
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+            count++;
+    }
+
+    if (count == 0)
+        printf("Number is prime");
+    else
+        printf("Number is not prime");
+
+    return 0;
+}
