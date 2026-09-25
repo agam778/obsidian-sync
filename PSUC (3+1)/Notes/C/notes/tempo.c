2823c7b11fcb57f0644dbfb7425e670a75db95e3
FNS Service <fns@email.com>
2026-09-25 13:36:32 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/C/notes/tempo.c
+++ PSUC (3+1)/Notes/C/notes/tempo.c
@@ -2,8 +2,19 @@
 
 int main()
 {
-    int n = 5, g = 1;
+    int n = 5;
     for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%4d", i);
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+
+    for (int i = n; i >= 1; i--)
     {
         for (int j = 1; j <= i; j++)
         {
