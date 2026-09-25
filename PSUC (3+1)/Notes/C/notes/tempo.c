b2563ee2fd56da327eedc6bceb4fa01c53d404e6
FNS Service <fns@email.com>
2026-09-25 11:56:47 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/C/notes/tempo.c
+++ PSUC (3+1)/Notes/C/notes/tempo.c
@@ -2,14 +2,5 @@
 
 int main()
 {
-    int a = 10, b = 12, c = 14;
-    if (a < b && a < c)
-        printf("a is smallest");
-
-    else if (b < a && b < c)
-        printf("b is smallest");
-    else
-    {
-        printf("c is smallest");
-    }
+    
 }
