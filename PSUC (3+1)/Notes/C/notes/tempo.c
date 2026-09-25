dd70f6d2412fb6cd1fcb1c500272b3c6a7ffc4ad
FNS Service <fns@email.com>
2026-09-25 12:19:17 +0800

    Update from Sync Service

--- PSUC (3+1)/Notes/C/notes/tempo.c
+++ PSUC (3+1)/Notes/C/notes/tempo.c
@@ -2,11 +2,11 @@
 
 int main()
 {
-    int n, d, place;
+    int n, d, place, result;
     printf("Enter a number: ");
     scanf("%d", n);
     while (n != 0)
         d = n % 10;
-
+        result = result + ()
 
 }
